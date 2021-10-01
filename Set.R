fffd96b289fa639ab79d84f3bd5121aa3fb7c999
何凌锋 <873140267@qq.com>
2021-10-02 00:55:56 +0800

    Update MSM, SPLM

--- Set.R
+++ Set.R
@@ -1,4 +1,5 @@
 xaringanExtra::use_panelset()
 xaringanExtra::style_panelset_tabs(font_family = "inherit")
 
-knitr::opts_chunk$set(out.width = "100%", cache = T)
+knitr::opts_chunk$set(out.width = "100%", cache = T, 
+                      message = FALSE, warning = FALSE)
