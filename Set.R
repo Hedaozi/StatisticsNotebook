f3ede7e3bb8523f328bcd97e57fc19a9cce05c2d
何凌锋 <873140267@qq.com>
2021-10-08 11:33:34 +0800

    Update MSM

--- Set.R
+++ Set.R
@@ -3,3 +3,7 @@ xaringanExtra::style_panelset_tabs(font_family = "inherit")
 
 knitr::opts_chunk$set(out.width = "100%", cache = T, 
                       message = FALSE, warning = FALSE)
+
+library(showtext)
+font_add("Centaur", "CENTAUR.TTF")
+showtext_auto()
